fca9e4034812a6bc20e35c70b5a7bbc62c03f71c
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-01-26 20:45:32 +0800

    Printed the sorted output for insertion sorting

--- insertion.py
+++ insertion.py
@@ -7,8 +7,22 @@ def insertion(array):
         key = array[step]
         j = step - 1
 
-        while j >= 0 and key < array[j]:
+        #comparing key with other elements on the 
+        #left until the algorithm finds a smaller element
+        while j >= 0 and key < array[j]: 
             array[j + 1] = array[j]
             j = j - 1
 
         array[j +1] = key
+
+data = [81,13,29,57,86,56,44,83,78,53]
+insertion(data)
+print('-----------------------------------------------')
+print()
+print("--> Given data: [81,13,29,57,86,56,44,83,78,53]")
+print()
+print("-----------------------------------------------")
+print()
+print(f"---> Sorted data through bubble algorithm:\n{data}")
+print()
+print("-----------------------------------------------")

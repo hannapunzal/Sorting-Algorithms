a650c4a3db3343b03ab493af563a058bfd562e6f
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-01-26 21:34:08 +0800

    Fixed the error--Index out of range

--- merge.py
+++ merge.py
@@ -3,29 +3,34 @@
 # code reference: programiz
 
 def merge(array):
+    if len(array) > 1 :
+        r = len(array)//2
+        L = array[:r]
+        M = array[r:]
 
-    r = len(array)//2
-    L = array[:r]
-    M = array[r:]
+        merge(L)
+        merge(M)
 
-    merge(L)
-    merge(M)
+        i = j = k = 0
 
-    i = j = k = 0
+        while i < len(L) and j < len(M):
+            if L[i] < M[j]:
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = M[j]
+                j += 1
+            k += 1
 
-    while i < len(L) and j < len(M):
-        if L[i] < M [j]:
+        while i < len(L):
             array[k] = L[i]
             i += 1
-        else:
+            k += 1
+
+        while j < len(M):
             array[k] = M[j]
             j += 1
-        k += 1
-    
-    while i < len(L):
-        array[k] = L[i]
-        j += 1
-        k += 1
+            k += 1
 
 def print_list(array):
     for i in range(len(array)):
@@ -44,7 +49,7 @@ if __name__ == '__main__':
     print()
     print("-----------------------------------------------")
     print()
-    print(f"---> Sorted data through merge algorithm:\n{data}")
+    print(f"---> Sorted data through merge algorithm:\n{array}")
     print()
     print("-----------------------------------------------")
 

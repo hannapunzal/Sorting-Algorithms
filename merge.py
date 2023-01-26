a856e0772dd726bb4d6d6586405357e46b834c60
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-01-26 21:29:12 +0800

    Complete code but there's an error encountered

---
+++ merge.py
@@ -0,0 +1,51 @@
+# MERGE SORTING
+# DATA : 81,13,29,57,86,56,44,83,78,53
+# code reference: programiz
+
+def merge(array):
+
+    r = len(array)//2
+    L = array[:r]
+    M = array[r:]
+
+    merge(L)
+    merge(M)
+
+    i = j = k = 0
+
+    while i < len(L) and j < len(M):
+        if L[i] < M [j]:
+            array[k] = L[i]
+            i += 1
+        else:
+            array[k] = M[j]
+            j += 1
+        k += 1
+    
+    while i < len(L):
+        array[k] = L[i]
+        j += 1
+        k += 1
+
+def print_list(array):
+    for i in range(len(array)):
+        print(array[i], end="")
+
+    print()
+
+if __name__ == '__main__':
+    array = [81,13,29,57,86,56,44,83,78,53]
+
+    merge(array)
+
+    print('-----------------------------------------------')
+    print()
+    print("--> Given data: [81,13,29,57,86,56,44,83,78,53]")
+    print()
+    print("-----------------------------------------------")
+    print()
+    print(f"---> Sorted data through merge algorithm:\n{data}")
+    print()
+    print("-----------------------------------------------")
+
+
